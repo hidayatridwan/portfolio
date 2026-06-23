a5f2bec022e4226da2e2133abc93a68fcd7f5699
Ridwan Hidayat <ridwan.nurulhidayat@gmail.com>
2026-06-23 15:16:54 +0700

    add: framework axum

--- src/data/skills.ts
+++ src/data/skills.ts
@@ -7,7 +7,7 @@ export const skills: SkillGroup[] = [
   { category: 'Languages', items: ['PHP', 'JavaScript', 'Dart', 'Rust'] },
   {
     category: 'Frameworks',
-    items: ['Node.js', 'Laravel', 'Slim', 'CodeIgniter', 'Express', 'Svelte', 'React', 'Flutter', 'jQuery'],
+    items: ['Node.js', 'Laravel', 'Slim', 'CodeIgniter', 'Express', 'Svelte', 'React', 'Flutter', 'jQuery', 'Axum'],
   },
   { category: 'Databases', items: ['MySQL', 'Postgres', 'SQL Server', 'MongoDB', 'Redis'] },
   { category: 'Message Broker', items: ['RabbitMQ'] },
